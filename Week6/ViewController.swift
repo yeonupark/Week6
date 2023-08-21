784909a48e3f888df03c4fb0fc4419ed3ef8be0f
yeonupark <idepix@naver.com>
2023-08-21 11:58:47 +0900

    Initial Commit

---
+++ Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week6
+//
+//  Created by 마르 on 2023/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
